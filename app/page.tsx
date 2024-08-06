5f5927249459f8ea64d31dc84b469f5f6842612f
Benzazoua <billalben252@gmail.com>
2024-08-06 22:46:09 +0100

    Install Shadcn UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="p-2">
       <h1>Hello World</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
