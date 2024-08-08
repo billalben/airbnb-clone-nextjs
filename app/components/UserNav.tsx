2a145a91a241c750bb430c6dcd2236873f9e5894
Benzazoua <billalben252@gmail.com>
2024-08-08 21:03:10 +0100

    Setting up Auth with Kinde

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+/* eslint-disable @next/next/no-img-element */
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,27 +7,36 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { MenuIcon } from "lucide-react";
-import Image from "next/image";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 
 export async function UserNav() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <div className="flex items-center gap-x-3 rounded-full border px-2 py-2 lg:px-4 lg:py-2">
           <MenuIcon className="h-6 w-6 lg:h-5 lg:w-5" />
 
-          <Image
-            src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+          <img
+            src={
+              user?.picture ??
+              "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+            }
             alt="Image of the user"
-            width={24}
-            height={24}
             className="hidden h-8 w-8 rounded-full lg:block"
           />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[200px]">
-        {true ? (
+        {user ? (
           <>
             <DropdownMenuItem>
               <form className="w-full">
@@ -53,16 +62,16 @@ export async function UserNav() {
             </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
-              <Button>Logout</Button>
+              <LogoutLink className="w-full">Logout</LogoutLink>
             </DropdownMenuItem>
           </>
         ) : (
           <>
             <DropdownMenuItem>
-              <Button>Register</Button>
+              <RegisterLink className="w-full">Register</RegisterLink>
             </DropdownMenuItem>
             <DropdownMenuItem>
-              <Button>Login</Button>
+              <LoginLink className="w-full">Login</LoginLink>
             </DropdownMenuItem>
           </>
         )}
